863f2d227f2011890eada26abac7f9c6bf017651
toki513 <tokitahmidkhan@gmail.com>
2024-06-24 20:55:05 +0600

    updated

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let audioElement = new Audio('1.mp3')
+
+
+let songs = [
+    {songName : "salam-e" , filePath : "song/1.mp3" , coverPath : "covers/1.jpg"},
+    {songName: "sala-e", filePath : "song/1.mp3", coverPath: "covers/2.jpg"}, 
+    {songName : "salam- e", filePath : "song/2.mp3", coverPath: "covers/3.jpg"},
+    {songName: "salad-e", filePath: "song/4.mp3", coverPath: "covers/4.jpg"},
+]
